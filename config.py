426190d580fc1a4c926ea750a8a0c2851d798cb7
roppan <61796905+roppan@users.noreply.github.com>
2020-08-05 14:27:16 -0400

    Adding WeatherBobRob code

---
+++ config.py
@@ -0,0 +1,8 @@
+# Twitter API Credentials
+consumer_key = 'yqAa5NfGdwLrmSckwzwbXbDpC'
+consumer_secret = '0SA6Agde34xWbmflATwmEQvDvVLnXqWqSvwCkHjo246D71nCXB'
+access_token = '1182156927113019393-9SL4sYNnQP2J7rui8FVInGWMMRlGaU'
+access_token_secret = '3eB9DLPmxrp3b1p5na9CKsxaTCu3mXpwGi5AC9GiaCNmL'
+# OpenWeatherMap API Key
+weather_api_key = 'ed9ab2e28cee00874214ba2e2f1068a1'
+
